741e2f3126e22141103394232b9e54c83bc2db31
tactnician <tactnician@gmail.com>
2020-01-22 15:12:22 -0500

    island notes

--- projects/island/island.py
+++ projects/island/island.py
@@ -8,3 +8,18 @@
 #            [1, 1, 0, 0, 0]]
 # island_counter(islands) # returns 4
 
+import util 
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
+let arry = []
+for i in islands: 
+    for j in islands[i]:
+        if j = 1:
+            new_tup = (i, j)
+            arry.push(new_tup)
+
